dbb8fe9630d77e8f6b8c95cc381f1dfbff298b95
Dolan Bright <vilinamogili423@gmail.com>
2020-07-10 10:33:35 +0300

    Fixed errors

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,10 +3,14 @@ import "./ImageList.scss";
 
 export default class ImageList extends Component {
   render() {
-    const images = this.props.images.map((image, index) => {
+    const images = this.props.images.map((image) => {
       return (
-        <a key={index} href={image.urls.regular} title={image.description}>
-          <img key={index} src={image.urls.regular} alt={image.description} />
+        <a key={image.id} href={image.urls.regular} title={image.description}>
+          <img
+            key={image.id}
+            src={image.urls.regular}
+            alt={image.description}
+          />
         </a>
       );
     });
